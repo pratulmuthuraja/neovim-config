3a2f0465987d25cb224e1833c39f1a19bb98b173
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2023-03-24 21:27:43 -0500

    Add keymaps for plugins - nvim-tree

--- init.lua
+++ init.lua
@@ -2,4 +2,5 @@ require("pratulmuthuraja.plugins-setup")
 require("pratulmuthuraja.core.options")
 require("pratulmuthuraja.core.keymaps")
 require("pratulmuthuraja.core.colorscheme")
-
+require("pratulmuthuraja.plugins.comment")
+require("pratulmuthuraja.plugins.nvim-tree")
--- lua/pratulmuthuraja/core/keymaps.lua
+++ lua/pratulmuthuraja/core/keymaps.lua
@@ -6,3 +6,17 @@ local keymap = vim.keymap
 keymap.set("i", "jk", "<ESC>")
  
 keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- tmux split keymaps
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+-- plugin keymaps
+--
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
